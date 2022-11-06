decb88fc481561aefcff2f12199cd598bd1ade1e
sravan kumar malla <90055494+Sravankumar998@users.noreply.github.com>
2022-11-06 12:20:41 +0530

    deleyted

--- Pagination.js
+++
@@ -1,9 +0,0 @@
-export default function Pagination({ gotoNextPage , gotoPrevPage }) {
-  return (
-    <div className='buttons'>
-      {gotoPrevPage && <button className="btn btn1" onClick={gotoPrevPage}>PREVIUS</button>}
-      {gotoNextPage && <button className="btn btn2" onClick={gotoNextPage}>NEXT</button>}
-    </div>
-    
-  )
-}
